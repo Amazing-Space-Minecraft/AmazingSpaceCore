37ae27a0ec2912b8f9509283045ee9c861703adc
Gold <37640079+ItsAlreadyGold@users.noreply.github.com>
2022-04-03 21:00:47 -0400

    Updated tier 3 hyperdrive multiblock

--- src/main/kotlin/net/starlegacy/feature/multiblock/hyperdrive/HyperdriveMultiblockClass3.kt
+++ src/main/kotlin/net/starlegacy/feature/multiblock/hyperdrive/HyperdriveMultiblockClass3.kt
@@ -24,36 +24,51 @@ object HyperdriveMultiblockClass3 : HyperdriveMultiblock() {
 			}
 
 			for (y in 0..1) y(y) {
-				x(-1).anyGlassPane()
-				x(+0).anyGlass()
-				x(+1).anyGlassPane()
+				x(-2).anyGlassPane()
+				x(-1).anyGlass()
+				x(+0).sponge()
+				x(+1).anyGlass()
+				x(+2).anyGlassPane()
 			}
 		}
 
 		z(+1) {
 			y(-1) {
-				x(+0).emeraldBlock()
+				x(-2).ironBlock()
+				x(-1).diamondBlock()
+				x(+0).ironBlock()
+				x(+1).diamondBlock()
+				x(+2).ironblock()
 			}
 
 			for (y in 0..1) y(y) {
-				x(-1).ironBlock()
-				x(+0).sponge()
-				x(+1).ironBlock()
+				x(-2).sponge()
+				x(-1).diamondBlock()
+				x(+0).ironBlock()
+				x(+1).diamondBlock()
+				x(+2).sponge()
 			}
 		}
 
 		z(+2) {
+			y(-1) {
+				x(0).ironBlock()
+			}
+
 			for (y in 0..1) y(y) {
-				x(-1).anyGlassPane()
-				x(+0).anyGlass()
-				x(+1).anyGlassPane()
+				x(-2).anyGlassPane()
+				x(-1).anyGlass()
+				x(+0).sponge()
+				x(+1).anyGlass()
+				x(+2).anyGlassPane()
 			}
 		}
 	}
 
 	override fun buildHopperOffsets() = listOf(
-		Vec3i(x = -1, y = -1, z = +1),    // left hopper
-		Vec3i(x = +1, y = -1, z = +1),    // right hopper
-		Vec3i(x = +0, y = -1, z = +2)     // rear hopper
+		Vec3i(x = -1, y = -1, z = +0),    // front left hopper
+		Vec3i(x = +1, y = -1, z = +0),    // front right hopper
+		Vec3i(x = -1, y = -1, z = +2),    // rear left hopper
+		Vec3i(x = +1, y = -1, z = +2)     // rear left hopper
 	)
 }
