8c80f9f202da4c6b025a9575ca7bd72f0b9a9722
Gold <37640079+ItsAlreadyGold@users.noreply.github.com>
2022-04-03 20:33:11 -0400

    Updated basic nav computer multiblock

--- src/main/kotlin/net/starlegacy/feature/multiblock/navigationcomputer/NavigationComputerMultiblockAdvanced.kt
+++ src/main/kotlin/net/starlegacy/feature/multiblock/navigationcomputer/NavigationComputerMultiblockAdvanced.kt
@@ -1,65 +1,46 @@
 package net.starlegacy.feature.multiblock.navigationcomputer
 
 import net.starlegacy.feature.multiblock.MultiblockShape
+import net.starlegacy.feature.progression.advancement.SLAdvancement
 
-object NavigationComputerMultiblockAdvanced : NavigationComputerMultiblock() {
+object NavigationComputerMultiblockBasic : NavigationComputerMultiblock() {
+	override val advancement = SLAdvancement.NAV_COMPUTER_BASIC
+
 	override val signText = createSignText(
-		line1 = "&6Advanced",
+		line1 = "&aBasic",
 		line2 = "&8Navigation",
 		line3 = "&8Computer",
 		line4 = null
 	)
 
-	override val baseRange: Int = 50000
+	override val baseRange: Int = 25000
 
 	override fun MultiblockShape.buildStructure() {
 		z(+0) {
 			y(+0) {
-				x(-2).anyStairs()
 				x(-1).anyStairs()
-				x(+0).ironBlock()
+				x(+0).wireInputComputer()
 				x(+1).anyStairs()
-				x(+2).anyStairs()
 			}
 
 			y(+1) {
-				x(-2).ironBlock()
 				x(-1).anyGlassPane()
-				x(+0).anyGlassPane()
+				x(+0).anyGlass()
 				x(+1).anyGlassPane()
-				x(+2).ironBlock()
-			}
-			y(+2) {
-				x(-2).anyStairs()
-				x(-1).anyStairs()
-				x(+0).ironBlock()
-				x(+1).anyStairs()
-				x(+2).anyStairs()
 			}
 		}
 
 		z(+1) {
 			y(+0) {
-				x(-2).ironBlock()
-				x(-1).sponge()
+				x(-1).ironBlock()
 				x(+0).sponge()
-				x(+1).sponge()
-				x(+2).ironBlock()
+				x(+1).ironBlock()
 			}
 
 			y(+1) {
-				x(-2).ironBlock()
-				x(-1).sponge()
+				x(-1).anyGlassPane()
 				x(+0).diamondBlock()
-				x(+1).sponge()
-				x(+2).ironBlock()
-			}
-			y(+2) {
-				x(-2).ironBlock()
-				x(-1).sponge()
-				x(+0).sponge()
-				x(+1).sponge()
-				x(+2).ironBlock()
+				x(+1).anyGlassPane()
 			}
 		}
 	}
