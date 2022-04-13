dc224b173b9e8149fb37243afe4257d61c350090
Demopans <gao.jeff3@gmail.com>
2022-04-12 23:06:45 -0400

    fixes

--- src/main/kotlin/net/starlegacy/feature/multiblock/reactors/ReactorMultiblock.kt
+++ src/main/kotlin/net/starlegacy/feature/multiblock/reactors/ReactorMultiblock.kt
@@ -1,8 +1,19 @@
 package net.starlegacy.feature.multiblock.reactors
 
 import net.starlegacy.feature.multiblock.FurnaceMultiblock
+import net.starlegacy.feature.multiblock.Multiblock
+import net.starlegacy.feature.multiblock.MultiblockShape
 import net.starlegacy.feature.multiblock.PowerStoringMultiblock
 import net.starlegacy.feature.starship.active.ActiveStarship
 
 // I still need to work on this, just formatting stuff out for now
-class ReactorMultiblock(starship: ActiveStarship)
+abstract class ReactorMultiblock: Multiblock() {
+	override val name: String
+		get() = TODO("Not yet implemented")
+	override val signText: List<String>
+		get() = TODO("Not yet implemented")
+
+	override fun MultiblockShape.buildStructure() {
+		TODO("Not yet implemented")
+	}
+}
